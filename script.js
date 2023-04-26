2541076c48454d285b3c613de4eadf3c2ada33f9
PrasannaL15 <limayeprasanna15@gmail.com>
2023-04-26 14:25:30 +0530

    scrolltopadded

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+$('html,body').scrollTop(0);
 $(document).ready(function () {
+
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function () {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
